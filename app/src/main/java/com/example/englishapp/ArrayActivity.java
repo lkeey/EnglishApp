d9b5ae19f53b120ca1d8df5055f31bbb71855ada
l_keey14 <Leon062007@mail.ru>
2023-01-03 21:41:44 +0300

    Begin sending notifications and changing wallpaper

--- app/src/main/java/com/example/englishapp/ArrayActivity.java
+++ app/src/main/java/com/example/englishapp/ArrayActivity.java
@@ -1,5 +1,6 @@
 package com.example.englishapp;
 
+import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
@@ -17,6 +18,7 @@ import android.view.View;
 import android.widget.Toast;
 
 import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -26,9 +28,6 @@ public class ArrayActivity {
     private static Context context;
     public String[] enWords = {};
     public String[] ruWords = {};
-
-    private NotificationManager notificationManager;
-    private NotificationCompat.Builder notificationBuilder;
 
     ArrayActivity(String[] enWords, String[] ruWords) {
         this.enWords = enWords;
@@ -52,39 +51,18 @@ public class ArrayActivity {
         Toast.makeText(context, "Good2", Toast.LENGTH_SHORT).show();
     }
 
-    public void showNotification(Context context, String title, String message, Intent intent, int reqCode) {
+    public void showNotification(int reqCode) {
 
-        PendingIntent pendingIntent = PendingIntent.getActivity(context, reqCode, intent, PendingIntent.FLAG_ONE_SHOT);
-        String CHANNEL_ID = "channel_name";// The id of the channel.
-
-        notificationBuilder = new NotificationCompat.Builder(context, CHANNEL_ID)
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(context.getApplicationContext(), "channel_name")
                 .setSmallIcon(R.drawable.ic_launcher_background)
-                .setContentTitle(title)
-                .setContentText(message)
-                .setWhen(System.currentTimeMillis())
-                .setAutoCancel(false)
-                .setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION))
-                .setContentIntent(pendingIntent);
+                .setContentTitle("HI")
+                .setContentText("HI2")
+                .setWhen(System.currentTimeMillis());
 
-        notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            CharSequence name = "Channel Name";// The user-visible name of the channel.
-            int importance = NotificationManager.IMPORTANCE_HIGH;
-            NotificationChannel mChannel = new NotificationChannel(CHANNEL_ID, name, importance);
-            notificationManager.createNotificationChannel(mChannel);
-        }
-        notificationManager.notify(reqCode, notificationBuilder.build()); // 0 is the request code, it should be unique id
-
-        Log.d("showNotification", "showNotification: " + reqCode);
+        Notification notification = notificationBuilder.build();
+        NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(context.getApplicationContext());
+        notificationManagerCompat.notify(reqCode, notification);
     }
 
-//    public int setStream (InputStream bitmapData,
-//                          Rect visibleCropHint,
-//                          boolean allowBackup)
-
-//    private Context getApplicationContext() {
-//        return ArrayActivity.context;
-//    }
 }
 
--- app/src/main/java/com/example/englishapp/MainActivity.java
+++ app/src/main/java/com/example/englishapp/MainActivity.java
@@ -3,8 +3,25 @@ package com.example.englishapp;
 import static android.view.WindowManager.LayoutParams.FLAG_FULLSCREEN;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
+import android.annotation.SuppressLint;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.WallpaperManager;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.media.RingtoneManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
@@ -12,10 +29,20 @@ import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.Toast;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.security.acl.NotOwnerException;
+
 public class MainActivity extends AppCompatActivity {
 
     private long backPressedTime;
 
+    public NotificationManager notificationManager;
+    public NotificationManagerCompat notificationManagerCompat;
+    public Notification notification;
+
+    @SuppressLint("WrongThread")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,6 +50,78 @@ public class MainActivity extends AppCompatActivity {
 
         Window window = getWindow();
         window.setFlags(FLAG_FULLSCREEN, FLAG_FULLSCREEN);
+
+//        send notification
+        int reqCode = 1;
+        Intent intentNew = new Intent(getApplicationContext(), MainActivity.class);
+
+        String CHANNEL_ID = "channel_name";// The id of the channel.
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = "Channel Name";// The user-visible name of the channel.
+            int importance = NotificationManager.IMPORTANCE_HIGH;
+            NotificationChannel mChannel = new NotificationChannel(CHANNEL_ID, name, importance);
+
+            notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(mChannel);
+        }
+
+        notificationManagerCompat = NotificationManagerCompat.from(this);
+
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, "channel_name")
+                .setSmallIcon(R.drawable.ic_launcher_background)
+                .setContentTitle("Title")
+                .setContentText("Something text")
+                .setWhen(System.currentTimeMillis() + 3000);
+
+        notification = notificationBuilder.build();
+        notificationManagerCompat.notify(reqCode, notification);
+
+//        change wallpaper
+        int[] colors = new int[300*300];
+
+        Bitmap bitmapAlpha1 = Bitmap.createBitmap(colors, 300, 300, Bitmap.Config.ARGB_8888);
+        Bitmap bitmapAlpha2 = BitmapFactory.decodeResource(getResources(), R.drawable.main_img_user);
+//
+//        String gText = "HI";
+//
+//        Canvas canvas = new Canvas(bitmapAlpha2); //Создаем Canvas на его основе
+//        Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);// Создаем кисть со сглаживанием
+//
+//        paint.setColor(Color.rgb(0, 0, 0)); //Ставим цвет
+//        paint.setTextSize(24); //Ставим размер текста в пикселях
+//
+//        //делаем некоторые действия чтобы выяснить куда рисовать
+//        Rect bounds = new Rect();//объект, в который положим размеры, которые займет наш текст
+//
+//        int x = (bitmapAlpha2.getWidth() - bounds.width())/2; //зная ширину и высоту текста, вычисляем координаты верхнего левого угла надписи в координатах вашего фото, поменяйте на то что вам нужно
+//        int y = (bitmapAlpha2.getHeight() + bounds.height())/2;
+//
+//        try {
+//            paint.getTextBounds(gText, 0, gText.length(), bounds); //вычисляем размеры текста, нарисованного этой кистью
+//
+//            canvas.drawText(gText, x, y, paint);//рисуем ваш текст в ваших координатах вашей кистью
+//            FileOutputStream fos = null; //создаем поток для записи в итоговый файл
+//
+//            fos = new FileOutputStream("new_file");
+//            bitmapAlpha2.compress(Bitmap.CompressFormat.JPEG, 90, fos);//пишем в этот поток конвертированное в JPEG ил PNG изображение
+//            fos.close();//закрываем поток
+//        } catch (FileNotFoundException e) {
+//            e.printStackTrace();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+
+
+        Toast.makeText(this, "Good", Toast.LENGTH_SHORT).show();
+
+        try {
+
+            WallpaperManager.getInstance(this).setBitmap(bitmapAlpha2);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
 
         // go to account
         Button btnStart = findViewById(R.id.btnLogging);
@@ -53,14 +152,18 @@ public class MainActivity extends AppCompatActivity {
                             new String[]{"awd"}, new String[]{"AWd"}
                     );
 
-//                    Toast.makeText(MainActivity.this, "Good3", Toast.LENGTH_SHORT).show();
-//
-//                    ac.createWallPaper();
-//
+                    Toast.makeText(MainActivity.this, "Good2", Toast.LENGTH_SHORT).show();
 
-                    int reqCode = 1;
-                    Intent intentNew = new Intent(getApplicationContext(), MainActivity.class);
-                    ac.showNotification(getApplicationContext(), "Title", "Message", intentNew, reqCode);
+//                    ac.showNotification(reqCode);
+
+//                    NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(getApplicationContext(), "channel_name")
+//                            .setSmallIcon(R.drawable.ic_launcher_background)
+//                            .setContentTitle("HI")
+//                            .setContentText("HI2")
+//                            .setWhen(System.currentTimeMillis());
+//
+//                    notification = notificationBuilder.build();
+//                    notificationManagerCompat.notify(reqCode, notification);
 
                     Toast.makeText(MainActivity.this, "Good4", Toast.LENGTH_SHORT).show();
 
@@ -69,7 +172,6 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
-
 
         //Pass the Exam
         Button btnCheck = findViewById(R.id.btnCheck);
