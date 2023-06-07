1c19aeb31f98070a912cb47b043bc17c4139d25c
l_keey14 <Leon062007@mail.ru>
2023-06-07 16:04:20 +0300

    send google api requests using Retrofit; update create word layouts; save and check data using repository

--- app/src/main/java/com/example/englishapp/managers/LocationManager.java
+++ app/src/main/java/com/example/englishapp/managers/LocationManager.java
@@ -1,47 +1,16 @@
 package com.example.englishapp.managers;
 
-import static com.example.englishapp.database.Constants.KEY_LATITUDE;
-import static com.example.englishapp.database.Constants.KEY_LOCATION;
-import static com.example.englishapp.database.Constants.KEY_LONGITUDE;
-import static com.example.englishapp.database.Constants.LOCAL_BROADCAST_ACTION;
-
-import android.Manifest;
-import android.app.Activity;
 import android.content.Context;
-import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.os.Build;
-import android.os.Looper;
 import android.provider.Settings;
 import android.text.TextUtils;
 import android.util.Log;
 
-import androidx.core.app.ActivityCompat;
-import androidx.localbroadcastmanager.content.LocalBroadcastManager;
-
-import com.example.englishapp.database.DataBase;
-import com.example.englishapp.interfaces.CompleteListener;
-import com.google.android.gms.common.api.ResolvableApiException;
-import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.location.LocationCallback;
-import com.google.android.gms.location.LocationRequest;
-import com.google.android.gms.location.LocationResult;
-import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.location.LocationSettingsRequest;
-import com.google.android.gms.location.LocationSettingsResponse;
-import com.google.android.gms.location.Priority;
-import com.google.android.gms.location.SettingsClient;
-import com.google.android.gms.tasks.Task;
-
 public class LocationManager {
 
     private static final String TAG = "ManagerLocation";
-    private static final int REQUEST_CHECK = 10_000;
     private static LocationManager instance = null;
     private Context context;
-    private FusedLocationProviderClient fusedLocationProviderClient;
-    private static LocationRequest locationRequest;
-    private LocationCallback locationCallback;
 
     private LocationManager () {}
 
@@ -58,144 +27,13 @@ public class LocationManager {
     }
 
     private void init(Context context) {
-        Log.i(TAG, "init");
-
         this.context = context;
-
-        this.fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(context);
-        Intent intent = new Intent(LOCAL_BROADCAST_ACTION);
-//        StringBuilder stringBuilder = new StringBuilder();
-
-        Log.i(TAG, "init 2");
-
-        try {
-            locationCallback = new LocationCallback() {
-
-                @Override
-                public void onLocationResult(LocationResult locationResult) {
-                Log.i(TAG, "send intent");
-
-                super.onLocationResult(locationResult);
-
-                if (locationResult == null) {
-
-                    Log.i(TAG, "null result");
-                    return;
-
-                }
-
-//                // send a lot of requests
-//                for (Location location : locationResult.getLocations()) {
-//                    Log.i(TAG, "Location - " + location.getLatitude() + " - " + location.getLongitude());
-//
-////                        stringBuilder.setLength(0);
-////                        stringBuilder.append("Time: " + System.currentTimeMillis() + " Latitude: " + location.getLatitude() + " Longitude: " + location.getLongitude());
-//
-//                    intent.putExtra(KEY_LATITUDE, String.valueOf(location.getLatitude()));
-//                    intent.putExtra(KEY_LONGITUDE, String.valueOf(location.getLongitude()));
-//
-//                    LocalBroadcastManager.getInstance(context).sendBroadcast(intent);
-//                }
-
-                // send only one request
-                intent.putExtra(KEY_LATITUDE, String.valueOf(locationResult.getLastLocation().getLatitude()));
-                intent.putExtra(KEY_LONGITUDE, String.valueOf(locationResult.getLastLocation().getLongitude()));
-
-                Log.i(TAG, "send intent to broadcast - " + locationResult.getLastLocation().getLatitude() + " - " + locationResult.getLastLocation().getLongitude());
-
-                // update user position in firebase
-                DataBase.updateUserGeoPosition(locationResult.getLastLocation().getLatitude(), locationResult.getLastLocation().getLongitude(), new CompleteListener() {
-                    @Override
-                    public void OnSuccess() {
-                        // send intent to show notification
-
-                        intent.putExtra(KEY_LOCATION, "Location Work - Successfully Updated");
-
-                        LocalBroadcastManager.getInstance(context).sendBroadcast(intent);
-
-                        Log.i(TAG, "Location was updated");
-                    }
-
-                    @Override
-                    public void OnFailure() {
-                        // can not update user's location in firebase
-
-                        intent.putExtra(KEY_LOCATION, "Location Work - Not Updated");
-
-                        LocalBroadcastManager.getInstance(context).sendBroadcast(intent);
-
-                        Log.i(TAG, "Can not update user geo position");
-                    }
-                });
-                }
-            };
-
-            Log.i(TAG, "init: 5");
-        } catch (Exception e) {
-            Log.i(TAG, "Err " + e.getMessage());
-        }
-    }
-
-    public void createLocationRequest() {
-        Log.i(TAG, "createLocationRequest");
-
-        // deprecated
-//        locationRequest = LocationRequest.create();
-//        locationRequest.setInterval(30_000);
-//        locationRequest.setFastestInterval(30_000);
-//        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-
-        locationRequest = new LocationRequest.Builder(Priority.PRIORITY_BALANCED_POWER_ACCURACY, 60*1000)
-                .setWaitForAccurateLocation(true)
-                .setMinUpdateIntervalMillis(1000)
-                .build();
-
-        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
-                .addLocationRequest(locationRequest);
-
-        SettingsClient client = LocationServices.getSettingsClient(context);
-
-        Task<LocationSettingsResponse> task = client.checkLocationSettings(builder.build());
-
-        task.addOnSuccessListener(locationSettingsResponse -> Log.i(TAG, "success task"));
-
-        task.addOnFailureListener(e -> {
-            if (e instanceof ResolvableApiException) {
-
-                try {
-
-                    ResolvableApiException resolvableApiException = (ResolvableApiException) e;
-                    resolvableApiException.startResolutionForResult((Activity) context, REQUEST_CHECK);
-
-                } catch (Exception ex) {
-                    Log.i(TAG, "error - " + ex.getMessage());
-                }
-
-            }
-        });
-    }
-
-    public void startLocationUpdates() {
-        Log.i(TAG, "startLocationUpdates");
-
-        if (ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            Log.i(TAG, "null - permission");
-
-        } else {
-            Log.i(TAG, "okey");
-
-            fusedLocationProviderClient.requestLocationUpdates(
-                    locationRequest,
-                    locationCallback,
-                    Looper.getMainLooper()
-            );
-        }
     }
 
     public boolean isLocationEnabled() {
         Log.i(TAG, "isLocationEnabled");
 
-        int locationMode = 0;
+        int locationMode;
         String locationProviders;
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
